74e9cc9b133ee334ed916bcd3090321fefe3e643
Lukas <lukasimbm@gmail.com>
2020-07-18 22:44:32 +0300

    update Fee and extending classes

--- Models/CashOutLegalFee.js
+++ Models/CashOutLegalFee.js
@@ -3,8 +3,8 @@ const Fee = require('../Models/Fee');
 class CashInFee extends Fee {
 
     calculateFee(user, transaction){
-        let fee = transaction.operation.amount * 0.0003;
-        if(fee > 5) fee = 5.00;
+        let fee = transaction.operation.amount * 0.003;
+        if(fee < 0.50) fee = 0.50;
         return this.roundUp(fee,2);
     }
 
--- Models/Fee.js
+++ Models/Fee.js
@@ -6,7 +6,15 @@ module.exports = class Fee {
 
     roundUp(num, precision) {
         precision = Math.pow(10, precision);
-        return Math.ceil(num * precision) / precision;
+        const roundedFee = Math.ceil(num * precision) / precision;
+        return this.format(roundedFee);
+    }
+
+    format(num) {
+        return new Intl.NumberFormat('en-IN', {
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2,
+        }).format(num);
     }
 
 };
