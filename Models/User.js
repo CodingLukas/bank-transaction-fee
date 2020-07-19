6e35dbf1deed11e8009f58e4fa6eb300f720efa3
Lukas <lukasimbm@gmail.com>
2020-07-19 11:35:22 +0300

    add amount and last transaction date to User.js

--- Models/User.js
+++ Models/User.js
@@ -3,6 +3,8 @@ module.exports = class User {
     constructor(id) {
        this.id = id;
        this.transactions = {};
+       this.amount = 0;
+       this.lastTransactionDate;
     }
 
     getId(){
